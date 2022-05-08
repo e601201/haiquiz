620fa679969b9aa938a5bd73eb64e33232b848a5
e601201 <k.arthur1111@gmail.com>
2022-05-08 17:32:02 +0900

    [ADD]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'quizes#index'
   get '/faqs', to: 'staticpages#faqs'
   get '/about', to: 'staticpages#about'
-  resources :quizes, shallow: true do
+  resources :quizes, except: %i[edit update], shallow: true do
     resources :answer, only: %i[index create]
   end
 end
